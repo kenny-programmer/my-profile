4d8ed71ab090cc61c0222d3c7223295f75d244ee
kenny-programmer <roxas.vctr@gmail.com>
2025-02-26 13:28:18 +0800

    Analytics applied

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "@/styles/globals.css";
 import { Inter } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider";
 import { cn } from "@/lib/utils";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -32,6 +33,7 @@ export default function RootLayout({
         >
           {children}
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   );
